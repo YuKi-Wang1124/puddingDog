ed780dd675f2edd548417ec787a2355b13b0b0ac
yuki <wkiki1124@gmail.com>
2021-08-08 00:53:44 +0800

    Initial Commit

---
+++ puddingDog/puddingDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  puddingDogApp.swift
+//  puddingDog
+//
+//  Created by 王昱淇 on 2021/8/8.
+//
+
+import SwiftUI
+
+@main
+struct puddingDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
